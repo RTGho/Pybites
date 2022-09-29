be5879da1cbd06cf87a99b8ce49fe0a043f3a7d3
RTGho <RTGholson@gmail.com>
2022-09-29 12:02:27 -0500

    added exercise folders

---
+++ 102/pybites_bite102/submissions/save1_nopass.py
@@ -0,0 +1,17 @@
+VALID_COLORS = ['blue', 'yellow', 'red']
+
+
+def print_colors():
+   while True:
+       color = input()
+        if color == 'quit':
+            print("bye")
+            break
+        if color in VALID_COLORS: 
+            print(color.lower())
+        elif color != VALID_COLORS:
+            print("Not a valid color")
+            print('bye')
+        else:
+            continue
+        pass
